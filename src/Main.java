ec8522bc172d9ab6342deb3c11ad32f1b1baa10c
TheSpicyBoi <nicklas.wehlauer@gmail.com>
2023-11-27 16:50:07 +0100

    Main klasse erstellt die zentrallager initialisiert und mit sachen auffüllt, standorte erstellt und auffüllt, sachen verkauft und dann lagerbestand checkt

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,35 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        // Erstellung von Getränken
+        Getraenke cola = new Getraenke("Cola", 24, 10);
+        Getraenke limonade = new Getraenke("Limonade", 30, 15);
 
+        // Erstellung von Standorten
+        Standort standort1 = new Standort("Lager1", 100);
+        Standort standort2 = new Standort("Lager2", 150);
 
-        }
+        // Erstellung des Zentrallagers
+        Zentrallager zentrallager = new Zentrallager("Zentrale");
+        zentrallager.initialeLagerbestaende();
+
+        // Verschicken von Getränken vom Zentrallager zu Standorten
+        zentrallager.verschicke(standort1, cola, 5);
+        zentrallager.verschicke(standort2, limonade, 3);
+
+        // Überprüfen der Lagerbestände in den Standorten
+        System.out.println("Lagerbestände in Standort 1:");
+        standort1.lagerbestandChecken();
+        System.out.println("\nLagerbestände in Standort 2:");
+        standort2.lagerbestandChecken();
+
+        // Verkauf von Getränken von Standorten
+        standort1.sachenVerkaufen(20);
+
+        // Überprüfen der aktualisierten Lagerbestände nach dem Verkauf
+        System.out.println("\nLagerbestände in Standort 1 nach Verkauf:");
+        standort1.lagerbestandChecken();
+
+        // Weitere Aktionen im Zentrallager möglich, z.B. Zugriff auf andere Funktionen
+        // zentralLager.weitereFunktionImZentralLager();
     }
+}
--- src/Zentrallager.java
+++ src/Zentrallager.java
@@ -1,12 +1,30 @@
 import java.util.ArrayList;
+import java.util.List;
+
 //diese Klasse initialisiert die lagerbestände in de,Ö_n standorten!!! das ist nicht zum nachschicken GEDACHT!!!!
 public class Zentrallager {
     private String name;
-    private ArrayList<Lagerbestand> lagerbestaende;
+
+    public List<Lagerbestand> zentrallagerbestand;
+
+    Getraenke wasser = new Getraenke("Wasser", 20, 30);
+    Getraenke saft = new Getraenke("Saft", 12, 25);
+
+    public void initialeLagerbestaende() {
+        Getraenke wasser = new Getraenke("Wasser", 20, 30);
+        Getraenke saft = new Getraenke("Saft", 12, 25);
+
+        Lagerbestand wasserLager = new Lagerbestand(wasser, 50);
+        Lagerbestand saftLager = new Lagerbestand(saft, 40);
+
+        zentrallagerbestand.add(wasserLager);
+        zentrallagerbestand.add(saftLager);
+    }
+
 
     public Zentrallager(String name) {
         this.name = name;
-        this.lagerbestaende = new ArrayList<>();
+        this.zentrallagerbestand = new ArrayList<>();
     }
 
     public void verschicke(Standort standort, Getraenke getraenk, int anzahlKaesten) {
@@ -15,7 +33,7 @@ public class Zentrallager {
         standort.lagerbestand.add(neuerLagerbestand);
 
         // Sucht den entsprechenden Lagerbestand im Zentrallager
-        for (Lagerbestand lager : lagerbestaende) {
+        for (Lagerbestand lager : zentrallagerbestand) {
             if (lager.getGetraenk() == getraenk) {
                 // Aktualisiert den gefundenen Lagerbestand im Zentrallager mit der neuen Anzahl an Einzelflaschen
                 lager.setAnzahlEinzelflaschen(lager.getAnzahlEinzelflaschen() + neuerLagerbestand.getAnzahlEinzelflaschen());
